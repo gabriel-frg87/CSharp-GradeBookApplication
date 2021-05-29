c83568d281bf093096ef954d8cd3f5a5d09f5862
Gabriel <gabrielfolea1987@yahoo.com>
2021-05-29 20:14:52 +0300

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,27 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new  InvalidOperationException();
+            }
+
+            switch (averageGrade)
+            {
+                case var d when d >= 80:
+                    return 'A';
+                case var d when (d >= 60 && d < 80):
+                    return 'B';
+                case var d when (d >= 40 && d < 60):
+                    return 'C';
+                case var d when (d >= 20 && d < 40):
+                    return 'D';
+                default:
+                    return 'F';
+            }
+        }
     }
 }
